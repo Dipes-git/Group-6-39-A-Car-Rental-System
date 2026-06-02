a29408f007d62867be33334b7ddbc9ed5f8507f7
aastha001518 <abastakoti4321@gmail.com>
2026-06-02 10:15:00 +0545

    feat(model): add getters and setters to Location entity

--- src/model/Location.java
+++ src/model/Location.java
@@ -24,4 +24,28 @@ public class Location {
         this.city = city;
         this.address = address;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
